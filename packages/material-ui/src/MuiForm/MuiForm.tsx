f9eb52be866a109dc8d961086c9342025773f0b5
Martin Raedlinger <mr@formatlos.de>
2022-02-17 13:21:51 +0100

    remove legacy material-ui

--- packages/material-ui/src/MuiForm/MuiForm.tsx
+++
@@ -1,8 +0,0 @@
-import { StatelessComponent } from 'react';
-import { withTheme, FormProps } from '@rjsf/core';
-
-import Theme from '../Theme';
-
-const MuiForm: React.ComponentClass<FormProps<any>> | StatelessComponent<FormProps<any>> = withTheme(Theme);
-
-export default MuiForm;
